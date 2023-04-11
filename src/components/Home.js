4afcb887a9e5b339beda424faed552126953b137
RaveBlue <my-raveblue21@yahoo.com>
2023-04-10 20:28:49 -0600

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,23 +5,13 @@ import "./Home.css";
 const Home = () => {
   return (
     <div id="home-container">
-      <h1 className="header">Amazoom</h1>
+      <h1 className="header">Divine</h1>
       
       <h2 className="subheader">
-        We are Cars, <u>Paradise</u>
+        Protein Ice-cream 
       </h2>
-      {/* <div className="btn-group">
-      <button>Trucks</button>
-       <button>Sedans</button>
-        <button>SUVs</button>
-     </div>
-      <div className="btn-group">
-       <button>Electric</button>
-       <button>Car</button>
-      <button>Car</button>
-     </div> */}
-       
-       <div className="textbox">Affordable Cars!</div>
+      
+       <div className="textbox">Healthy Ice-cream!</div>
        
       
     
